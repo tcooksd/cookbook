989ad1ce6e7599b1ccdf64235d8aa98ae9c93835
tcooksd <tcooksd@gmail.com>
2017-09-07 06:36:28 +0000

    adding test cookbooks

---
+++ ubuntu/README.md
@@ -0,0 +1,4 @@
+# ubuntu
+
+TODO: Enter the cookbook description here.
+
---
+++ ubuntu/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: ubuntu
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package "apache2" do
+  action :install
+end
+
+
